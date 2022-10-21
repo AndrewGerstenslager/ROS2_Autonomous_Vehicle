d20475fafdeb31777d7dddd03156e91a1d55871f
Andrew Gerstenslager <gerstead@mail.uc.edu>
2022-10-21 16:23:19 -0400

    Added ROS2 Node to motor library - refactoring

--- motor_library/README.md
+++ motor_library/README.md
@@ -7,8 +7,13 @@
 
 ## PYTHON:
 The python section has a ROS2 node that communicates to and from the ROS2 network.
+
+This code is located at /src/motor_contol/motor_control/motor_subscriber_node.py
+
 The python section also uses serial communication to talk to and from the Arduino
 over its Serial port (USB).
+
+This library is located at /src/motor_contol/motor_control/libraries/serial_utils.py
 
 ***NOTE: We did this because ROS2 developers at this time are terrible and the ROS2 Arduino libraries are all broken, suck, and don't support basic Arduino Mega/Uno boards***
 
@@ -28,9 +33,17 @@ to the Arduino by the way we check the port.description to contain "Arduino".
 
 ## To use this library:
 
-Install arduino_main.ino to the Arduino using the Arduino IDE
+1. Install arduino_main.ino or any similar file to the Arduino using the Arduino IDE
 
-***NOTE: If Serial1 is not recognized, make sure you have Arduino mega selected as the target board type***
+***NOTE: If Serial1 is not recognized, make sure you have "Arduino Mega" selected as the target board type***
 
-Run the python code from a computer connected via USB to the Arduino
+2. Build the package using "colcon build" command in "/dokalman/motor_library" directory
+
+3. Run ". install/local_setup.bash" to source the package
+
+4. Run "ros2 pkg list | grep motor" to verify that motor_control comes up
+
+5. Finally Run "ros2 run motor_control arduino_node" to start the package
+
+ 
 
---
+++ motor_library/src/motor_control/motor_control/motor_subscriber_node.py
@@ -0,0 +1,67 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from libraries.serial_utils import *
+
+
+class Arduino_Node(Node):
+    '''
+        This class is a ros2 node that communicates to the arduino    
+    '''
+    def __init__(self):
+        super().__init__('Arduino_Node')
+        
+        #subscribe to the turn message
+        self.subscription = self.create_subscription(
+            String,
+            'turn_speed',
+            self.turn_callback,
+            10)
+
+        #subscribe to the drive message
+        self.subscription = self.create_subscription(
+            String,
+            'drive_speed',
+            self.drive_callback,
+            10)
+
+        self.subscription  # prevent unused variable warning
+
+    def turn_callback(self, msg):
+        '''
+            Creates the turn message for the Arduino to
+            send to the Kangaroo Motor Controller. \n
+            Message is from the "turn_speed" topic
+        '''
+        turn_msg = f"t,s{msg.data}"
+        self.get_logger().info(f"Sent: '{turn_msg}' to Arduino")
+        #TODO: Add code to utilize serial library
+
+    def drive_callback(self, msg):
+        '''
+            Creates the drive message for the Arduino to
+            send to the Kangaroo Motor Controller.\n
+            Message is from the "drive_speed" topic
+        '''
+        drive_msg = f"d,s{msg.data}"
+        self.get_logger().info(f"Sent: '{drive_msg}' to Arduino")
+        #TODO: Add code to utilize serial library
+
+    #TODO: Add a subscriber that can call a reset to the arduino to restablish the serial connection
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    arduino_node = Arduino_Node()
+
+    rclpy.spin(arduino_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    arduino_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
