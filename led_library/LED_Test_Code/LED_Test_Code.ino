4a7b1d86b7e9e05e39cd65818b5a448ae63828e8
J Roth <thejroths@gmail.com>
2023-03-11 13:40:39 -0500

    LED changes

--- led_library/LED_Test_Code/LED_Test_Code.ino
+++ led_library/LED_Test_Code/LED_Test_Code.ino
@@ -1,6 +1,8 @@
 #include <FastLED.h>
 
+
 #define LED_PIN     12
+#define POWER_PIN   13
 #define NUM_LEDS    16
 
 CRGB leds[NUM_LEDS];
@@ -18,13 +20,18 @@ String WaitForInput(String setcolor) {
 
 void setred () {
   for (int i = 0; i <= NUM_LEDS; i++) {
-    leds[i] = CRGB ( 125, 0, 10);
+    leds[i] = CRGB ( 255, 0, 0);
     delay(50);
   
   }
   FastLED.show();
 }
-
+void setblue () {
+  for (int i = 0; i <= NUM_LEDS; i++) {
+    leds[i] = CRGB ( 0, 0, 255);
+  }
+  FastLED.show();
+}
 void setyellow (){
   for (int i = 0; i <= NUM_LEDS; i++) {
     leds[i] = CRGB ( 255, 255, 0);
@@ -35,7 +42,7 @@ FastLED.show();
 
 void setgreen (){
   for (int i = 0; i <= NUM_LEDS; i++) {
-    leds[i] = CRGB ( 0, 128, 0);
+    leds[i] = CRGB ( 0, 255, 0);
     delay(20);
 }
 FastLED.show();
@@ -49,18 +56,16 @@ void setblack () {
 FastLED.show();
 }
 
-void setblue () {
-  for (int i = 19; i >= 0; i--) {
-    leds[i] = CRGB ( 10, 0, 125);
-    FastLED.show();
-  }
-}
+
 
 void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
   Serial.begin(9600);
   Serial.println("I'M ALIVE!");
 
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  setred();
 
 }
 
