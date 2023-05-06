0e3a48a269b24b486d34ae1c3597f125f6be3bdb
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2023-05-06 22:43:55 +0000

    dokalman test

---
+++ dokalman_library/source.sh
@@ -0,0 +1,13 @@
+#THIS IS A CUSTOM INITALIZATION FILE
+
+echo "BUILDING PACKAGE"
+colcon build --packages-select dokalman
+echo "SOURCING PACKAGE"
+. install/local_setup.bash
+
+echo "PACKAGES BUILT:"
+ros2 pkg list | grep dokalman
+echo "DONE"
+echo "EXECUTABLES:"
+ros2 pkg executables dokalman 
+ros2 launch dokalman display.launch.py
