4d880b5dadca22c791a623a215004f69e5dfb7d6
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2023-06-02 12:26:35 -0400

    modified arduino code

--- motor_library/arduino_code/arduino_main/arduino_main.ino
+++ motor_library/arduino_code/arduino_main/arduino_main.ino
@@ -1,129 +1,21 @@
 #include <FastLED.h>
-#include <SoftwareSerial.h>
 #define rc_pin_left_right 4
 #define rc_pin_up_down 5
 #define rc_pin_trainer_toggle 7
-#define rc_pin_power 13
+#define rc_pin_power 8
 #define LED_PIN     12
 #define NUM_LEDS    16
-#define RX_PIN 2
-#define TX_PIN 3
-
-SoftwareSerial softSerial(RX_PIN, TX_PIN);
 
 CRGB leds[NUM_LEDS];
 
 int turn_calculated;
 int drive_calculated;
 int trainer_toggle;
-String driving_mode;
+int prev_trainer_toggle;
 int controller_tolerance = 25;
-
-String write_read(String data, bool send_back = 0){
-  /*
-   * This method is designed to send data to the kangaroo 
-   * and return the appropriate response to the sender
-   */
-  softSerial.println(data);
-  
-  //UNCOMMENT LINE BELOW TO SEE THE SENT MESSAGE
-  //Serial.println(s);
-
-  //get response
-  String response = softSerial.readString();
-  
-  //Send response back to sender
-  if(send_back){
-    Serial.println(response);
-  }
-  
-  return response;
-}
-
-
-
-void self_drive_control(){
-  /*
-   * This method is used to receive data over the serial port from the NUC
-   * 
-   * We determine what command the NUC is sending, and then we execute that
-   * command then send the NUC back an appropriate response
-   * 
-   */
-  String com_usb = Serial.readString();
-  
-  if (com_usb != ""){
-    if(com_usb.charAt(0) == 'd'){
-      //Drive Command
-      softSerial.println(com_usb);
-      write_read("d,gets", true);
-    }
-    else if(com_usb.charAt(0) == 't' ){
-      //Turn Command
-      softSerial.println(com_usb);
-      //send speed back back to NUC
-      write_read("t,gets", true);
-    }
-    else if(com_usb.charAt(0) == 's'){
-      softSerial.println("d,s0");
-      softSerial.println("t,s0");
-      Serial.println("STOPPED");
-    }
-    else{
-      //Serial.println("COMMAND NOT RECOGNIZED");
-    }
-    
-    com_usb = "";
-  
-  }
-}
-
-
-void rc_control(){
-  /*
-   * This method updates the drive_calculated and turn_calculated
-   * values for us to access and write to the motors
-   * 
-   */
-   
-  String turn_setup = "t,s";
-  String drive_setup = "d,s";
-  int left_right_stick;
-  int up_down_stick;
-   
-  trainer_toggle = pulseIn(rc_pin_trainer_toggle, HIGH);
-
-  //If toggle switch is on setting "0"
-  //or the lowest setting
-  if(trainer_toggle > 1600){
-    left_right_stick = 1487 - pulseIn(rc_pin_left_right, HIGH);
-    //filter out inputs that are close to 0
-    if (left_right_stick > (-1 * controller_tolerance) && 
-                            left_right_stick < controller_tolerance){
-      turn_calculated = 0;
-    }
-    else{
-      turn_calculated = left_right_stick * 2;
-    }
-  
-    //filter out inputs that are close to 0
-    up_down_stick = 1574 - pulseIn(rc_pin_up_down, HIGH);
-    if (up_down_stick > (-1 * controller_tolerance) && 
-                            up_down_stick < controller_tolerance){
-      drive_calculated = 0;
-    }
-    else{
-      drive_calculated = up_down_stick * 2;
-    }
-  }
-
-  turn_setup.concat(turn_calculated);
-  drive_setup.concat(drive_calculated);
-  softSerial.println(turn_setup);
-  softSerial.println(drive_setup);
-
-}
-
+int led_counter = 0;
+bool led_on = true;
+bool self_drive_flag = false;
 
 // ____________________LED CODE__________________________
 void setred () {
@@ -165,18 +57,160 @@ void setblack () {
 FastLED.show();
 }
 
+//________________SERIAL COMMUNICATION______________________
+
+String write_read(String data, bool send_back = 0){
+  /*
+   * This method is designed to send data to the kangaroo 
+   * and return the appropriate response to the sender
+   */
+  Serial1.println(data);
+  
+  //UNCOMMENT LINE BELOW TO SEE THE SENT MESSAGE
+  //Serial.println(s);
+
+  //get response
+  String response = Serial1.readString();
+  
+  //Send response back to sender
+  if(send_back){
+    Serial.println(response);
+  }
+  
+  return response;
+}
+
+//__________________DRIVE MODES_________________________________
+
+void self_drive_control(){
+  /*
+   * This method is used to receive data over the serial port from the NUC
+   * 
+   * We determine what command the NUC is sending, and then we execute that
+   * command then send the NUC back an appropriate response
+   * 
+   */
+  String com_usb = Serial.readString();
+  if(self_drive_flag){//if in autonomous mode
+    //led control code for flashing
+    led_counter++;
+    if(led_counter == 30){
+      if(led_on){
+        led_on = false;
+      }
+      else{
+        led_on = true;
+      }
+      led_counter = 0;
+    }
+    if(led_on){
+      setgreen();
+    }
+    else{
+      setblack();
+    }
+  }
+  else{
+    setgreen();
+  }
+  
+  if (com_usb != ""){
+    //Check to start or stop autonomous mode to 
+    //listen to commands from serial port
+    if(com_usb == "START"){
+      self_drive_flag = true;
+    }
+    else if(com_usb == "STOP"){
+      self_drive_flag = false;
+    }
+
+    if(self_drive_flag){
+      
+      //read and execute command
+      if(com_usb.charAt(0) == 'd'){
+        //Drive Command
+        Serial1.println(com_usb);
+        write_read("d,gets", true);
+      }
+      else if(com_usb.charAt(0) == 't' ){
+        //Turn Command
+        Serial1.println(com_usb);
+        //send speed back back to NUC
+        write_read("t,gets", true);
+      }
+      else if(com_usb.charAt(0) == 's'){
+        Serial1.println("d,s0");
+        Serial1.println("t,s0");
+        Serial.println("STOPPED");
+      }
+      else{
+        //Serial.println("COMMAND NOT RECOGNIZED");
+      }
+    }
+    
+    com_usb = "";//clear this variable
+  }
+}
+
+
+void rc_control(){
+  /*
+   * This method updates the drive_calculated and turn_calculated
+   * values for us to access and write to the motors
+   * 
+   */
+   
+  String turn_setup = "t,s";
+  String drive_setup = "d,s";
+  int left_right_stick;
+  int up_down_stick;
+   
+  trainer_toggle = pulseIn(rc_pin_trainer_toggle, HIGH);
+
+  //If toggle switch is on setting "0"
+  //or the lowest setting
+ // if(trainer_toggle > 1600){
+    left_right_stick = 1487 - pulseIn(rc_pin_left_right, HIGH);
+    //filter out inputs that are close to 0
+    if (left_right_stick > (-1 * controller_tolerance) && 
+                            left_right_stick < controller_tolerance){
+      turn_calculated = 0;
+    }
+    else{
+      turn_calculated = left_right_stick * 2;
+    }
+  
+    //filter out inputs that are close to 0
+    up_down_stick = 1574 - pulseIn(rc_pin_up_down, HIGH);
+    if (up_down_stick > (-1 * controller_tolerance) && 
+                            up_down_stick < controller_tolerance){
+      drive_calculated = 0;
+    }
+    else{
+      drive_calculated = up_down_stick * 5;
+    }
+  Serial.println(left_right_stick);
+  Serial.println(up_down_stick);
+  turn_setup.concat(turn_calculated);
+  drive_setup.concat(drive_calculated);
+  Serial1.println(turn_setup);
+  Serial1.println(drive_setup);
+
+}
+
 
 //______________________SETUP HARDWARE CODE______________________
 void wait_for_rc(){
-  softSerial.println("t,s0");
-  softSerial.println("d,s0");
+  Serial1.println("t,s0");
+  Serial1.println("d,s0");
     setyellow();
     digitalWrite(rc_pin_power, LOW);
-    delay(2000);
+    delay(500);
     digitalWrite(rc_pin_power, HIGH);
     trainer_toggle = pulseIn(rc_pin_trainer_toggle, HIGH);
     while (trainer_toggle < 100){
       trainer_toggle = pulseIn(rc_pin_trainer_toggle, HIGH);
+      Serial.println(trainer_toggle);
     }
 }
 
@@ -215,17 +249,16 @@ void initialize_kangaroo(){
   //Send These values to "wake up" motors
   //NOTE: idk why we need this but it won't take drive and turn
   //      commands until we send these
-  softSerial.println("t,s100");
-  softSerial.println("d,s100");
-  softSerial.println("t,s0");
-  softSerial.println("d,s0");
+  Serial1.println("t,s100");
+  Serial1.println("d,s100");
+  Serial1.println("t,s0");
+  Serial1.println("d,s0");
   
 }
 
 
 //_____________________SETUP________________________________________
 void setup() {
-  Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   
   //Setup controller inputs
@@ -238,12 +271,12 @@ void setup() {
   
   //Begin Serial ports
   Serial.begin(9600);
-  softSerial.begin(9600);
+  Serial1.begin(9600);
 
   //Set timeout - important because readString() will wait until 10ms of data is read in
   //default value is 1000ms (1 sec) and is really slow otherwise
   Serial.setTimeout(10);
-  softSerial.setTimeout(10);
+  Serial1.setTimeout(10);
 
   initialize_kangaroo();
 
@@ -260,36 +293,25 @@ void setup() {
 void loop() {
   trainer_toggle = pulseIn(rc_pin_trainer_toggle, HIGH);
   
-  if(trainer_toggle > 1600){
+  
+  if(trainer_toggle - prev_trainer_toggle > 1600){
     setblue();
     rc_control();
-    driving_mode = "RC";
-    //setgreen();
-    Serial.println("RC MODE");
+    //Serial.println("RC MODE");
   }
-  else if(trainer_toggle > 1400){
-    setgreen();
-    if(driving_mode == "RC"){
-      softSerial.println("d,s0");
-      softSerial.println("t,s0");
-    }
+  else if(trainer_toggle - prev_trainer_toggle > 1400){
     self_drive_control();
-    driving_mode = "SD";
-    //setblue();
-    Serial.println("SELF DRIVING MODE");
+    //Serial.println("SELF DRIVING MODE");
   }
   else if(trainer_toggle == 0){
-    softSerial.println("d,s0");
-    softSerial.println("t,s0");
-    Serial.println("NO CONTROLLER DETECTED");
     wait_for_rc();
   }
   else{
     setred();
-    softSerial.println("d,s0");
-    softSerial.println("t,s0");
-    driving_mode = "ST";
-    Serial.println("STOPPED");
+    Serial1.println("d,s0");
+    Serial1.println("t,s0");
+    //Serial.println("STOPPED");
   }  
+  
   
 }
