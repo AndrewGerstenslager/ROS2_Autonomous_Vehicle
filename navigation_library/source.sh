03d565ff0e09ad2fbf17dbfc207c8b2267660adb
= <=>
2024-02-05 23:21:28 +0000

    added framework for navigation library

---
+++ navigation_library/source.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# CHANGE LINE BELOW FOR UPDATING THE PACKAGE NAME
+export PACKAGE_NAME='basic_navigation'
+# ADD LINE BELOW TO DEFINE THE PACKAGE TYPE
+export PACKAGE_TYPE='python' # 'cpp' or 'python' depending on your package
+
+# Function to print in red
+print_red() {
+    echo -e "\033[0;31m$1\033[0m"
+}
+
+# Function to print in blue
+print_blue() {
+    echo -e "\033[0;34m$1\033[0m"
+}
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+print_blue "BUILDING PACKAGE"
+colcon build
+print_blue "SOURCING PACKAGE"
+source install/setup.bash
+
+print_blue "PACKAGES BUILT:"
+ros2 pkg list | grep ${PACKAGE_NAME}
+
+# Check for executables
+print_blue "EXECUTABLES:"
+EXECUTABLES=$(ros2 pkg executables ${PACKAGE_NAME})
+if [ -z "$EXECUTABLES" ]; then
+    print_red "None"
+else
+    echo "$EXECUTABLES"
+fi
+
+# Check for launch files
+print_blue "LAUNCH FILES:"
+LAUNCH_DIR="src/${PACKAGE_NAME}/launch" # Default path for C++ packages
+if [ "${PACKAGE_TYPE}" = "python" ]; then
+    LAUNCH_DIR="launch" # Adjust path for Python packages if necessary
+fi
+
+if [ -z "$(ls -A ${LAUNCH_DIR} 2>/dev/null)" ]; then
+    print_red "None"
+else
+    ls ${LAUNCH_DIR}
+fi
