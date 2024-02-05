74b74a27043df07d85a5f4f8e86f94746c70c7b5
= <=>
2024-02-05 00:49:02 +0000

    fixed and additions for vision library

--- vision_library/src/cpp_image_processing/launch/launch_single_stack.py
+++ vision_library/src/cpp_image_processing/launch/launch_single_stack.py
@@ -5,13 +5,6 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='cpp_image_processing', 
-            executable='live_feed', 
-            name='live_feed',
-            parameters=[{'device_path':'/dev/video2'}, 
-                        {'published_topic':'img_raw'}]
-        ),
-        Node(
-            package='cpp_image_processing', 
             executable='image_skew', 
             name='image_skew',
             parameters=[{'subscribed_topic':'img_raw'}, 
@@ -25,12 +18,5 @@ def generate_launch_description():
             parameters=[{'subscribed_topic': 'img_skewed'},
                         {'published_topic': 'img_processed'},
                         {"threshold_value":150}]
-        ),
-        Node(
-            package='cpp_image_processing',
-            executable='image_to_pointcloud',
-            name='image_to_pointcloud',
-            parameters=[{'subscribed_topic': 'img_processed'},
-                        {'published_topic': 'my_pointcloud'}]
         ),
     ])
