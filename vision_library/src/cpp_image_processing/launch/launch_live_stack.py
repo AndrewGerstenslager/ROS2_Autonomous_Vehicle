2fe2b5c1ad6eb0ae5500800572a0f3151b10ee53
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-03-28 19:25:52 +0000

    tried to make live camera feed working

--- vision_library/src/cpp_image_processing/launch/launch_live_stack.py
+++ vision_library/src/cpp_image_processing/launch/launch_live_stack.py
@@ -7,27 +7,33 @@ def generate_launch_description():
             package='cpp_image_processing', 
             executable='live_feed', 
             name='right_live_feed',
-            parameters=[{'serial':'5EBF6F8F'}, {'published_topic':'right_img_raw'}]
+            parameters=[{'serial_number':'A230401000215170'}, {'published_topic':'right_img_raw'}]
+        ),
+        Node(
+            package='cpp_image_processing', 
+            executable='live_feed', 
+            name='front_live_feed',
+            parameters=[{'serial_number':'A231111000200156'}, {'published_topic':'front_img_raw'}]
         ),
         Node(
             package='cpp_image_processing', 
             executable='live_feed', 
             name='left_live_feed',
-            parameters=[{'serial':'B1CF6F8F'}, {'published_topic':'left_img_raw'}]
+            parameters=[{'serial_number':'A231111000209494'}, {'published_topic':'left_img_raw'}]
         ),
 
-        Node(
-            package='cpp_image_processing',
-            executable='image_threshold',
-            name='right_line_threshold',
-            parameters=[{'subscribed_topic': 'right_img_raw'},
-                        {'published_topic': 'right_img_processed'}]
-        ),
-        Node(
-            package='cpp_image_processing',
-            executable='image_threshold',
-            name='left_line_threshold',
-            parameters=[{'subscribed_topic': 'left_img_raw'},
-                        {'published_topic': 'left_img_processed'}]
-        ),
+        # Node(
+        #     package='cpp_image_processing',
+        #     executable='image_threshold',
+        #     name='right_line_threshold',
+        #     parameters=[{'subscribed_topic': 'right_img_raw'},
+        #                 {'published_topic': 'right_img_processed'}]
+        # ),
+        # Node(
+        #     package='cpp_image_processing',
+        #     executable='image_threshold',
+        #     name='left_line_threshold',
+        #     parameters=[{'subscribed_topic': 'left_img_raw'},
+        #                 {'published_topic': 'left_img_processed'}]
+        # ),
     ])
