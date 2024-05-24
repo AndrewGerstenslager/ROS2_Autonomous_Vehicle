55410e7f3c7d8998fc2e9becba06e55d9cdbe028
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-05-24 00:41:00 +0000

    added articubot code

---
+++ robot_library/src/articubot_one/launch/visualize_urdf.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import xacro
+import os
+
+
+def generate_launch_description():
+    package_name = 'articubot_one'
+
+    pkg_path = os.path.join(get_package_share_directory(package_name))
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config.toxml()}
+    
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    node_joint_state_publisher = Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+    )
+
+    node_rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory(package_name), 'config', 'visualize_urdf.rviz')]
+    )
+
+    return LaunchDescription([
+        node_robot_state_publisher,
+        node_joint_state_publisher,
+        node_rviz2,
+    ])
