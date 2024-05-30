f81e14992ea08ebe7ea85f7e995f8a434be342f4
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-05-29 20:27:43 -0400

    Added some useful docker bash scripts

---
+++ .devcontainer/build_container.sh
@@ -0,0 +1,2 @@
+# Use to build the DockerFile in this directory as a container with an image name for use in non-vscode environments:
+docker build -t dokalman_humble_ros2_image .
---
+++ open_docker_container.sh
@@ -0,0 +1,1 @@
+docker run --privileged --network=host -v $(pwd):/repo/dokalman -it dokalman_humble_ros2_image
